187cf403d83f4b91d3b07a9494623f5a26139c6d
Ralferre <raphael.l.ferreira@hotmail.com>
2023-07-22 19:35:30 -0300

    initial commit

---
+++ src/main/java/com/raphaelferreira/detectoreventapi/DetectorEventApiApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelferreira.detectoreventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorEventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectorEventApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphaelferreira/detectoreventapi/DetectorEventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelferreira.detectoreventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectorEventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
